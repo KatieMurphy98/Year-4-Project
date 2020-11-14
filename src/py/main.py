7fea82813515130e17f142c1162cf80e9218ccb2
Katie Murphy <katieamurphy@btinternet.com>
2020-11-14 11:42:28 +0000

    Started trying to do some python and loaded in some relevant libraries.

---
+++ src/py/main.py
@@ -0,0 +1,8 @@
+#python -m pip install -U pip
+#python -m pip install -U matplotlib
+
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+
+Dogs = pd.read_csv('C://Users//Katie//OneDrive//Uni_Work_Year4//Project//Data//AKC Breed Info.csv')
