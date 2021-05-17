2cfb1fc2e6ad78de645fe90e8c358eb74199f44b
Katie Murphy <katieamurphy@btinternet.com>
2021-05-17 23:42:27 +0100

    Start interview chapter

---
+++ markdown/chapters/Interviews/main.r
@@ -0,0 +1,15 @@
+
+\chapter{Coding of Visualisations}
+
+This chapter will discuss the programming behind the visualisations presented in the surveys, and present results of interviews discussing the opinions of programming specialists in relation to the codes. As previously discussed, two languages were used to create the plots; R and Python. Initially, the JavaScript D3 library was also considered, but discounted due to time constraints. The D3 library will, however, be briefly discussed here. The previous literature review discussed the overall theory behind the design of visualisation, but an additional brief review of literature regarding coding for visualisations will be undertaken here.
+
+
+\section{A Brief Review of Literature}
+
+
+
+\section{Background on semi-structured interviews}
+ A semi structured interview, as briefly discussed before
+
+
+
